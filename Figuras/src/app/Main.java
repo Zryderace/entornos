2cb24becd01685274793489d1dd29eec655740b0
Zryderace <147004178+Zryderace@users.noreply.github.com>
2024-03-19 16:16:39 +0100

    added main

---
+++ Figuras/src/app/Main.java
@@ -0,0 +1,17 @@
+package app;
+
+import modelos.Rectangulo;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Rectangulo rec = new Rectangulo(5,5);
+		
+		System.out.println(rec.area());
+		System.out.println(rec.perimetro());
+	
+	}
+
+}
